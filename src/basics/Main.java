b9da46470facc64d810dd1cdb790644e19a0f3f8
pradeepnithyan <pradeep331997@gmail.com>
2025-12-05 20:11:19 +0530

    program 1

---
+++ src/basics/Main.java
@@ -0,0 +1,22 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        int age=25;
+        String name="pangajam";
+        boolean isstudent=true;
+        double height=5.3;
+        System.out.println("name");
+        System.out.println("age");
+        System.out.println("height");
+        System.out.println("isstudent");
+    }
+}
+
+
+// output:
+// name
+// age
+// height
+// isstudent
