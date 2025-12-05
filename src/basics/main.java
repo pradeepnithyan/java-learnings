409cbadb5f2b9d05ba4a823206c1f6283e03bedc
pradeepnithyan <pradeep331997@gmail.com>
2025-12-05 20:24:55 +0530

    if else

---
+++ src/basics/main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int age=20;
+        if (age>=18){
+        System.out.println("eligible to vote");
+       }else{System.out.println("not eligible to vote");
+        }
+    }
+}
+//
